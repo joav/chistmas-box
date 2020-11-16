feb634cf49d189bd9d2c595cd5b927c31b2c8612
joav <jim.acero.valcarcel@gmail.com>
2020-11-16 00:41:44 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { WalletComponent } from './wallet/wallet.component';
-import { TransactionComponent } from './transaction/transaction.component';
-import { PersonsComponent } from './persons/persons.component';
-import { LoansComponent } from './loans/loans.component';
 import { CreateBoxComponent } from './create-box/create-box.component';
 import { SuccessCreateComponent } from './success-create/success-create.component';
 import { AccessBoxComponent } from './access-box/access-box.component';
@@ -19,10 +15,6 @@ import { SuccessWishComponent } from './success-wish/success-wish.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    WalletComponent,
-    TransactionComponent,
-    PersonsComponent,
-    LoansComponent,
     CreateBoxComponent,
     SuccessCreateComponent,
     AccessBoxComponent,
