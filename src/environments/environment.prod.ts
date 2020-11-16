e03938cbebb6d284fa3594fb7f5b1494562d0a01
joav <jim.acero.valcarcel@gmail.com>
2020-11-16 12:34:00 -0500

    Firebase conf and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDfK3rlvUa22vSBIQGoHcSCVC6tr6jsYGQ",
+    authDomain: "christmas-box.firebaseapp.com",
+    databaseURL: "https://christmas-box.firebaseio.com",
+    projectId: "christmas-box",
+    storageBucket: "christmas-box.appspot.com",
+    messagingSenderId: "101478229217",
+    appId: "1:101478229217:web:719842667262e06acc43a0"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDfK3rlvUa22vSBIQGoHcSCVC6tr6jsYGQ",
+    authDomain: "christmas-box.firebaseapp.com",
+    databaseURL: "https://christmas-box.firebaseio.com",
+    projectId: "christmas-box",
+    storageBucket: "christmas-box.appspot.com",
+    messagingSenderId: "101478229217",
+    appId: "1:101478229217:web:719842667262e06acc43a0"
+  }
 };
 
 /*
