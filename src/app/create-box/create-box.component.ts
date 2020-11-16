caabedaa1bdfc7e0de196434361c8f0f405479fa
joav <jim.acero.valcarcel@gmail.com>
2020-11-16 16:22:29 -0500

    Update create-box.component.ts

--- src/app/create-box/create-box.component.ts
+++ src/app/create-box/create-box.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { BoxService } from '../service/box.service';
+import { BoxService } from '../services/box.service';
 
 @Component({
   selector: 'app-create-box',
