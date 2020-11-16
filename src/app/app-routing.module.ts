5be799bbd58ca773792230d4edd7513e2dfef4c9
joav <jim.acero.valcarcel@gmail.com>
2020-11-16 12:41:14 -0500

    Fix deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from '/src/app/home/home.component';
-import { CreateBoxComponent } from '/src/app/create-box/create-box.component';
-import { SuccessCreateComponent } from '/src/app/success-create/success-create.component';
-import { BoxComponent } from '/src/app/box/box.component';
-import { AccessBoxComponent } from '/src/app/access-box/access-box.component';
-import { SuccessWishComponent } from '/src/app/success-wish/success-wish.component';
-import { WishComponent } from '/src/app/wish/wish.component';
+import { HomeComponent } from './home/home.component';
+import { CreateBoxComponent } from './create-box/create-box.component';
+import { SuccessCreateComponent } from './success-create/success-create.component';
+import { BoxComponent } from './box/box.component';
+import { AccessBoxComponent } from './access-box/access-box.component';
+import { SuccessWishComponent } from './success-wish/success-wish.component';
+import { WishComponent } from './wish/wish.component';
 
 const routes: Routes = [
     {
